f67de1f5c450d8353694f3d1fe53325876fcc476
Artur Molchanov <artur.molchanov@synesis.ru>
2017-01-12 16:54:05 +0300

    kubelet: Add cpu-conversion-factor option
    Use --cpu-conversion-factor to calculate effective number of CPU.
    <Effective number of CPU> = <Real number of CPU> * cpu-conversion-factor
    
    It will be extremely useful for heterogeneous configurations where same
    application should be run on nodes with different CPU speed.
    
    Update POD containers requests and limits for CPU using cpu-conversion-factor:
    <requests CPU> /= cpu-conversion-factor
    <limits CPU> /= cpu-conversion-factor

--- pkg/kubelet/kuberuntime/kuberuntime_container.go
+++ pkg/kubelet/kuberuntime/kuberuntime_container.go
@@ -36,6 +36,7 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	kubetypes "k8s.io/apimachinery/pkg/types"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/apimachinery/pkg/util/sets"
 	"k8s.io/kubernetes/pkg/api/v1"
 	runtimeapi "k8s.io/kubernetes/pkg/kubelet/apis/cri/v1alpha1/runtime"
@@ -224,6 +225,13 @@ func (m *kubeGenericRuntimeManager) generateLinuxContainerConfig(container *v1.C
 	var cpuShares int64
 	cpuRequest := container.Resources.Requests.Cpu()
 	cpuLimit := container.Resources.Limits.Cpu()
+
+	cpuRequest = resource.NewMilliQuantity(int64(float32(cpuRequest.MilliValue())/m.cpuConversionFactor),
+	resource.DecimalSI)
+
+	cpuLimit = resource.NewMilliQuantity(int64(float32(cpuLimit.MilliValue())/m.cpuConversionFactor),
+	resource.DecimalSI)
+
 	memoryLimit := container.Resources.Limits.Memory().Value()
 	oomScoreAdj := int64(qos.GetContainerOOMScoreAdjust(pod, container,
 		int64(m.machineInfo.MemoryCapacity)))
